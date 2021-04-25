fb96c377965dc55885d557f18386a62930836a4b
togunyale <togunyale26@ehotmail.com>
2021-04-24 21:36:01 -0400

    Update LocalHost Var

--- app/services/nba-api.services.ts
+++ app/services/nba-api.services.ts
@@ -1,5 +1,9 @@
 import { EventEmitter, Injectable, Output } from "@angular/core";
-import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpParams,
+} from "@angular/common/http";
 import { Observable } from "rxjs";
 import { apis } from "../shared/interface";
 
@@ -8,8 +12,7 @@ export class NbaApiService {
   @Output() requestSent = new EventEmitter<any>();
   @Output() resetSettings = new EventEmitter<any>();
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   notifyChartOfRequest(payload: any) {
     this.requestSent.emit(payload);
@@ -19,16 +22,18 @@ export class NbaApiService {
   }
 
   getTeamDetails(team_name: string): Observable<any> {
-    return this.http.get(`${apis.API_GK_URL}/teamDetails`, {
-      params: new HttpParams().set("team_name", team_name)
+    return this.http.get(`${apis.API_URL}/teamDetails`, {
+      params: new HttpParams().set("team_name", team_name),
     });
   }
 
   getShotChartDetails(payload: any): Observable<any> {
-    return this.http.get(`${apis.API_GK_URL}/team/shotChart`, {
-      params: new HttpParams().set("subject", payload.subject)
-        .set("qtr", payload.qtr).set("shotType", payload.shotType)
-        .set("timeInterval", payload.timeInterval)
+    return this.http.get(`${apis.API_URL}/team/shotChart`, {
+      params: new HttpParams()
+        .set("subject", payload.subject)
+        .set("qtr", payload.qtr)
+        .set("shotType", payload.shotType)
+        .set("timeInterval", payload.timeInterval),
     });
   }
 }
